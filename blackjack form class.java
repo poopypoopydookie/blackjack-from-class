22f1edddc48cbaa4a065881101683db9e6c1efd7
poopypoopydookie <122311063+poopypoopydookie@users.noreply.github.com>
2023-01-09 11:20:11 -0800

    Create blackjack form class.java

---
+++ blackjack form class.java
@@ -0,0 +1,16 @@
+public class Card {
+  public static String Felix = "23456789TJQKA";
+  private char suit;
+  private char val;
+  
+  public Card() {
+    // Get a random number between 0-12
+    // Assign the value at that index for "val"
+    
+    // repeat for suit
+  }
+  
+  
+  public String toString() {
+  // Return the string
+}
